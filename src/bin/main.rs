acda0a3e1e38c5ea19e68e5762dae5451d355186
dat58 <vtdat58@gmail.com>
2025-11-05 17:42:38 +0000

    feat: Add support for two buttons and different output
    This commit adds support for reading input from two buttons
    (GPIO5 and GPIO14).  The program now outputs different strings
    depending on the state of the buttons:
    - "13\r\n" if both are pressed (low)
    - "12\r\n" if only GPIO5 is pressed (low)
    - "03\r\n" if only GPIO14 is pressed (low)
    - "02\r\n" if neither is pressed (high)
    
    This allows for more complex input and control schemes.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -10,9 +10,9 @@ use embedded_hal::delay::DelayNs;
 use esp_hal::{
     clock::CpuClock,
     delay::Delay,
+    gpio::{Input, InputConfig, Pull},
     main,
     uart::{Config, Uart},
-    gpio::{Input, InputConfig, Pull},
 };
 use esp_println as _;
 
@@ -25,7 +25,6 @@ extern crate alloc;
 
 esp_bootloader_esp_idf::esp_app_desc!();
 
-
 #[main]
 fn main() -> ! {
     // generator version: 0.5.0
@@ -36,13 +35,30 @@ fn main() -> ! {
     esp_alloc::heap_allocator!(size: 64 * 1024);
 
     let mut delay = Delay::new();
-    
+
     let mut uart = Uart::new(peripherals.UART0, Config::default()).unwrap();
 
-    let button = Input::new(peripherals.GPIO5, InputConfig::default().with_pull(Pull::Up));
-    
+    let button1 = Input::new(
+        peripherals.GPIO5,
+        InputConfig::default().with_pull(Pull::Up),
+    );
+    let button2 = Input::new(
+        peripherals.GPIO14,
+        InputConfig::default().with_pull(Pull::Up),
+    );
+
     loop {
-        let data = if button.is_low() { "1\r\n" } else { "0\r\n" };
+        let low1 = button1.is_low();
+        let low2 = button2.is_low();
+        let data = if low1 && low2 {
+            "13\r\n"
+        } else if low1 && !low2 {
+            "12\r\n"
+        } else if !low1 && low2 {
+            "03\r\n"
+        } else {
+            "02\r\n"
+        };
         uart.write(data.as_bytes()).unwrap();
         uart.flush().unwrap();
         delay.delay_ms(2_u32);
